f212e9949a837eb75db696b9c6f3c8bb6200f215
John Gonzalez <johngonzalezv@gmail.com>
2023-05-08 01:58:41 -0500

    :sparkles: chat historico + dynamo realizado!

--- get_response.py
+++ get_response.py
@@ -3,13 +3,15 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 
 from langchain.schema import (
-    AIMessage,
     HumanMessage,
-    SystemMessage
+    SystemMessage,
 )
 
 from pydantic import BaseModel
 from tiktoken import encoding_for_model
+
+from uuid import uuid4
+from datetime import datetime as dt
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -21,7 +23,8 @@ class Message(BaseModel):
 
 
 def format_context(relevant_docs):
-    return " ".join([d.page_content for d in relevant_docs])
+    return {'text': ' '.join([d.page_content for d in relevant_docs]),
+            'sources': [d.metadata['source'] for d in relevant_docs]}
 
 
 def get_similar_docs(db, query, k):
@@ -31,11 +34,12 @@ def get_similar_docs(db, query, k):
 
 
 def get_system_message_prompt(context):
+    # print(context)
 
     prompt = """
         Actúa como un asesor del "banco de Bogotá".
-        Responde preguntas de manera cordial, precisa, detallada y con la mayor veracidad posible
-        Si tu no estás seguro de la respuesta diga "Disculpa, no sé"
+        Responde preguntas de manera cordial, precisa y con la mayor veracidad posible utilizando el contexto.
+        Si el contexto no te da la suficiente información responde "Disculpa, no sé, comúnícate a nuestra servilinea 01 8000 518 877"
 
         Contexto:
         {docs}
@@ -45,7 +49,8 @@ def get_system_message_prompt(context):
 
 
 def get_human_message_prompt(question, additional_context=None):
-    partA = 'Contexto adicional: {context}'
+    # print(additional_context)
+    partA = 'Utiliza este contexto adicional: {context}'
     partB = 'Pregunta: {question}'
 
     if additional_context:
@@ -62,20 +67,20 @@ def get_token_size(message, model_name='gpt-3.5-turbo'):
     return len(encoding.encode(message))
 
 
-def get_more_context(messages):
+def get_more_context(db, messages):
     # Tomamos el mensaje del sistema y último query del usuario
     system_message = messages[0].content
     latest_query = messages[-1].content
 
     # Revisamos cuantos tokens acumulan entre estos dos mensajes
     token_count = get_token_size(system_message + ' ' + latest_query)
-    print('NUM TOKENS:', token_count)
+    # print('NUM TOKENS:', token_count)
 
     # Incluimos la mayor cantidad de mensajes posible si no superan 1k tokens
     new_messages = []
     for message in reversed(messages[1:-1]):
         token_count += get_token_size(message.content)
-        print('NUM TOKENS:', token_count)
+        # print('NUM TOKENS:', token_count)
         if token_count > 1000:
             break
         new_messages.append(message.content)
@@ -87,53 +92,88 @@ def get_more_context(messages):
     # Agregue más contexto usando solo el último documento más importante. Todo: Si el documento ya está busque otro
     docs = get_similar_docs(db, query_text, k=1)
     # print('\n\n\nDOC\n\n\n'.join([d.page_content for d in docs]))
-    more_context = format_context(docs)
-    query_with_context = get_human_message_prompt(latest_query, more_context)
-    return query_with_context
+    context = format_context(docs)
+    more_context = context['text']
+    return {
+        'query_with_context': get_human_message_prompt(latest_query, more_context),
+        'sources': context['sources']
+    }
 
 
-def get_response_from_query(db, messages):
+async def get_response_from_query(db, messages):
     model_name = 'gpt-3.5-turbo'
     chat = ChatOpenAI(model_name=model_name, temperature=0, verbose=True)
+    def get_dt(): return dt.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+    default_session_id = uuid4()
 
+    # Inicializa conversación
     if len(messages) == 1:
         query = messages[0].content
         docs = get_similar_docs(db, query, k=3)
         # print('\n\n\nDOC\n\n\n'.join([d.page_content for d in docs]))
         context = format_context(docs)
-        system_message_prompt = get_system_message_prompt(context)
+        # sources = context['sources']  # Todo: Pensar como exportar estas fuentes. En los mensajes?
+        system_message_prompt = get_system_message_prompt(context['text'])
         human_message_prompt = get_human_message_prompt(query)
 
-        system_message = SystemMessage(content=system_message_prompt)
-        latest_query = [HumanMessage(content=human_message_prompt)]
+        additional_kwargs = messages[0].additional_kwargs
+        additional_kwargs = {
+            'id': additional_kwargs.get('id', uuid4()),
+            'datetime': additional_kwargs.get('datetime', get_dt()),
+            'session_id': additional_kwargs.get('session_id', default_session_id)
+        }
 
-        response = chat([system_message] + latest_query)
-        messages = [system_message] + messages
+        system_message = SystemMessage(
+            content=system_message_prompt,
+            additional_kwargs=additional_kwargs
+        )
 
+        previous_messages = [HumanMessage(
+            content=human_message_prompt,
+            additional_kwargs=additional_kwargs
+        )]
+
+        messages = [system_message] + previous_messages
+
+    # Continúa conversación
     else:
         token_limit = 4000
-        lastest_query_with_context = get_more_context(messages)
+        context = get_more_context(db, messages)
+        # sources = context['sources']
+        lastest_query_with_context = context['query_with_context']
 
         system_message = messages[0]
         num_tokens = get_token_size(
             system_message.content + ' ' + lastest_query_with_context)
-        print('NUM TOKENS:', num_tokens)
+        # print('NUM TOKENS:', num_tokens)
+        # print(additional_kwargs)
 
-        add_messages = [HumanMessage(content=lastest_query_with_context)]
+        previous_messages = [HumanMessage(content=lastest_query_with_context)]
         # En reversa agregue mensajes siempre y cuando no supere el límite de tokens
+
         for message in reversed(messages[1:-1]):
             num_tokens += get_token_size(message.content) + 4
-            print('NUM TOKENS:', num_tokens)
+            # print('NUM TOKENS:', num_tokens)
             if num_tokens > token_limit:
                 break
-            add_messages = [message] + add_messages
+            previous_messages = [message] + previous_messages
 
-        response = chat([system_message] + add_messages)
+        messages[-1].additional_kwargs = {
+            'id': messages[-1].additional_kwargs .get('id', uuid4()),
+            'datetime': messages[-1].additional_kwargs .get('id', get_dt()),
+            'session_id': messages[0].additional_kwargs.get('session_id', default_session_id)
+        }
 
+    response = chat([system_message] + previous_messages)
+    response.additional_kwargs = {
+        'id': uuid4(),
+        'datetime': get_dt(),
+        'session_id': messages[0].additional_kwargs.get('session_id', default_session_id)
+    }
     return messages + [response]
 
 
-db = FAISS.load_local('output/embeddings_faiss_index', OpenAIEmbeddings())
+# db = FAISS.load_local('output/embeddings_faiss_index', OpenAIEmbeddings())
 # question = 'Qué pasa si no pago mi deuda a tiempo?'
 # question = 'Cuál es la estructura corporativa del banco?'
 # question = 'Quiénes son sus principales ejecutivos?'
@@ -147,61 +187,61 @@ db = FAISS.load_local('output/embeddings_faiss_index', OpenAIEmbeddings())
 
 
 # 1. Inicialice con el mensaje del usuario
-message = Message(sender='user',
-                  text='Hola. Vivo en Bogotá. Necesito retirar dinero')
-
-# Message history trae el mensaje del sistema + usuario + rta ia (3 en total)
-message_history = get_response_from_query(
-    db, [HumanMessage(content=message.text)])
-print('='*80+'\nRESPUESTA\n'+'='*80 + '\n', message_history[-1])
-
-# 2. Continue con la convesación (entran 4 msj en total)
-message = Message(sender='user',
-                  text='Deseo retirar dinero en un cajero, específicamente en el Norte')
-
-message_history.append(HumanMessage(content=message.text))
-
-message_history = get_response_from_query(db, message_history)
-print('='*80+'\nRESPUESTA\n'+'='*80 + '\n', message_history[-1])
-
-# 3. Continue con la convesación (entran 5 msj en total)
-message = Message(sender='user',
-                  text='Me puedes dar la ubicación exacta de un cajero automático cerca a cedritos')
-
-message_history.append(HumanMessage(content=message.text))
-
-message_history = get_response_from_query(db, message_history)
-print('='*80+'\nRESPUESTA\n'+'='*80 + '\n', message_history[-1])
-
-# 4. Continue con la convesación (entran 6 msj en total)
-message = Message(sender='user',
-                  text='y tienes el horario de funcionamiento de algún cajero cerca?')
-
-message_history.append(HumanMessage(content=message.text))
-message_history = get_response_from_query(db, message_history)
-print('='*80+'\nRESPUESTA\n'+'='*80 + '\n', message_history[-1])
-
-
-# 5. Continue con la convesación (entran 7 msj en total)
-message = Message(sender='user',
-                  text='y oficinas de atención?, tienes la ubicación y horario?')
-
-message_history.append(HumanMessage(content=message.text))
-message_history = get_response_from_query(db, message_history)
-print('='*80+'\nRESPUESTA\n'+'='*80 + '\n', message_history[-1])
-
-# 6. Continue con la convesación (entran 8 msj en total) Error! el num dado es el 018000 912 227 (BBVA)
 # message = Message(sender='user',
-#                   text='Si por favor, dame el número de servicio al cliente')
+#                   text='Hola. Vivo en Bogotá. Necesito retirar dinero')
 
-message = Message(sender='user',
-                  text='Este número no es del banco de bogotá')
+# # Message history trae el mensaje del sistema + usuario + rta ia (3 en total)
+# message_history = get_response_from_query(db, [HumanMessage(content=message.text)])
+# print('='*80+'\nRESPUESTA\n'+'='*80 + '\n', message_history[-1])
 
-message_history.append(HumanMessage(content=message.text))
-message_history = get_response_from_query(db, message_history)
-print('='*80+'\nRESPUESTA\n'+'='*80 + '\n', message_history[-1])
+# # 2. Continue con la convesación (entran 4 msj en total)
+# message = Message(sender='user',
+#                   text='específicamente en el Norte')
+
+# message_history.append(HumanMessage(content=message.text))
+
+# message_history = get_response_from_query(db, message_history)
+# print('='*80+'\nRESPUESTA\n'+'='*80 + '\n', message_history[-1])
 
 
-print('Cantidad de mensajes en total:', len(message_history))
-# Todo: Manejar este error:   raise self.handle_error_response(
-# openai.error.InvalidRequestError: This model's maximum context length is 4097 tokens. However, your messages resulted in 4124 tokens. Please reduce the length of the messages.
+# # 3. Continue con la convesación (entran 5 msj en total)
+# message = Message(sender='user',
+#                   text='Me puedes dar la ubicación exacta de un cajero automático cerca a cedritos')
+
+# message_history.append(HumanMessage(content=message.text))
+
+# message_history = get_response_from_query(db, message_history)
+# print('='*80+'\nRESPUESTA\n'+'='*80 + '\n', message_history[-1])
+
+# # 4. Continue con la convesación (entran 6 msj en total)
+# message = Message(sender='user',
+#                   text='y tienes el horario de funcionamiento de algún cajero cerca?')
+
+# message_history.append(HumanMessage(content=message.text))
+# message_history = get_response_from_query(db, message_history)
+# print('='*80+'\nRESPUESTA\n'+'='*80 + '\n', message_history[-1])
+
+
+# # 5. Continue con la convesación (entran 7 msj en total)
+# message = Message(sender='user',
+#                   text='vivo cerca al barrio cedritos')
+
+# message_history.append(HumanMessage(content=message.text))
+# message_history = get_response_from_query(db, message_history)
+# print('='*80+'\nRESPUESTA\n'+'='*80 + '\n', message_history[-1])
+
+# # 6. Continue con la convesación (entran 8 msj en total) Error! el num dado es el 018000 912 227 (BBVA) o 01 8000 912345 (Bancolombia)
+# message = Message(sender='user',
+#                   text='Perdón tienes el número de servicio al cliente')
+
+# # message = Message(sender='user',
+# #                   text='Me das la fuente de donde obtuviste este número de teléfono?')
+
+# message_history.append(HumanMessage(content=message.text))
+# message_history = get_response_from_query(db, message_history)
+# print('='*80+'\nRESPUESTA\n'+'='*80 + '\n', message_history[-1])
+
+
+# print('Cantidad de mensajes en total:', len(message_history))
+# # Todo: Manejar este error:   raise self.handle_error_response(
+# # openai.error.InvalidRequestError: This model's maximum context length is 4097 tokens. However, your messages resulted in 4124 tokens. Please reduce the length of the messages.
--- main.py
+++ main.py
@@ -6,22 +6,101 @@ from get_response import get_response_from_query
 import uvicorn
 import os
 import boto3
-import uuid
+from uuid import uuid4
 from datetime import datetime as dt
-
 from dotenv import load_dotenv
+from typing import List, Optional
+from pydantic import BaseModel
+from botocore.exceptions import ClientError
+from langchain.schema import (
+    HumanMessage,
+    SystemMessage,
+    AIMessage
+)
+
+
+class Message(BaseModel):
+    session_id: Optional[str] = None
+    id: Optional[str] = None
+    datetime: Optional[str] = None
+    text: str
+    sender: str = 'user'
+
+
 load_dotenv()
 
 
-def save_QA_dynamodb(dynamo_table, pregunta, respuesta):
-    item = {
-        "id": str(uuid.uuid4()),
-        "datetime": dt.now().strftime("%Y-%m-%dT%H:%M:%S.%f"),
-        "pregunta": pregunta,
-        "respuesta": respuesta,
-    }
-    dynamo_response = dynamo_table.put_item(Item=item)
+def save_QA_dynamodb(dynamo_table, messages):
+    for message in messages:
+        item = {
+            "id": str(message['id']),
+            "datetime": message['datetime'],
+            "session_id": str(message['session_id']),
+            "text": message['text'],
+            "sender": message['sender'],
+        }
+        # dynamo_response = dynamo_table.put_item(Item=item)
+        try:
+            dynamo_response = dynamo_table.put_item(
+                Item=item,
+                ConditionExpression='attribute_not_exists(id)'
+            )
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
+                print(
+                    f"El mensaje con id {message['id']} ya existe en la base de datos.")
+            else:
+                raise e
     return dynamo_response
+
+
+def set_messages_to_chat(messages):
+    message_history = []
+    for message in messages:
+        message_chat = {
+            'content': message.text,
+            'additional_kwargs':  {
+                key: value
+                for key, value in {
+                    'id': message.id,
+                    'datetime': message.datetime,
+                    'session_id': message.session_id
+                }.items()
+                if value is not None
+            }
+        }
+        if message.sender == 'system':
+            message_chat = SystemMessage(**message_chat)
+        elif message.sender == 'user':
+            message_chat = HumanMessage(**message_chat)
+        elif message.sender == 'ai':
+            message_chat = AIMessage(**message_chat)
+        else:
+            raise ValueError('sender should be "system", "user" or "ai"')
+        message_history.append(message_chat)
+    return message_history
+
+
+def set_messages_to_api(messages):
+    message_history = []
+    for message in messages:
+        message_api = {
+            'id': message.additional_kwargs['id'],
+            'datetime': message.additional_kwargs['datetime'],
+            'session_id': message.additional_kwargs['session_id'],
+            'text': message.content,
+        }
+        if isinstance(message, SystemMessage):
+            message_api['sender'] = 'system'
+        elif isinstance(message, HumanMessage):
+            message_api['sender'] = 'user'
+        elif isinstance(message, AIMessage):
+            message_api['sender'] = 'ai'
+        else:
+            raise ValueError(
+                'message should be instance of "SystemMessage", "HumanMessage" or "AIMessage"')
+        message_history.append(message_api)
+    return message_history
 
 
 # Crea la instancia de FastAPI
@@ -36,17 +115,20 @@ session = boto3.Session(
 )
 
 dynamodb = session.resource("dynamodb")
-dynamo_table = dynamodb.Table("chatbot")
+dynamo_table = dynamodb.Table("chatbot_v2")
 
 
 # Define la ruta y función de la API para realizar preguntas
-@app.get("/preguntar/{pregunta}")
-async def preguntar(pregunta: str):
-    respuesta = get_response_from_query(db, pregunta)
+@app.post("/preguntas")
+async def preguntar(mensajes: List[Message]) -> dict:
+    message_history = set_messages_to_chat(mensajes)
+    message_history = await get_response_from_query(db, message_history)
+    message_history = set_messages_to_api(message_history)
     # Almacena la pregunta y la respuesta en DynamoDB
-    dynamo_out = save_QA_dynamodb(dynamo_table, pregunta, respuesta)
+    # print(message_history)
+    dynamo_out = save_QA_dynamodb(dynamo_table, message_history)
     print('Respuesta dynamo:', dynamo_out)
-    return {"pregunta": pregunta, "respuesta": respuesta}
+    return {"status": "success", "data": message_history}
 
 
 # Define la función principal para ejecutar la aplicación con uvicorn
